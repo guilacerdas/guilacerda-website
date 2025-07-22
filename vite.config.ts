387fea1ee88e9f8ddd5133036bdb8378b17d04ad
guilacerdas <98661584+guilacerdas@users.noreply.github.com>
2025-07-22 00:22:59 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: isGithubPages ? '/guilacerda-website/' : '/', 
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
